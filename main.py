bb9f12568fc5e051133c7b4ec180f5610ac39713
Keichi Takahashi <hello@keichi.dev>
2025-04-08 16:59:42 +0900

    Use multiprocessing

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import socket
 import struct
 import tempfile
 import time
-import threading
+import multiprocessing
 
 
 PORT = 8000
@@ -16,25 +16,20 @@ CMD_START = 1
 CMD_END = 2
 
 
+def receiver(sock, is_running, n_received_total):
+    print(f"Connected receiver local: {sock.getsockname()} remote: {sock.getpeername()}")
+
+    buf = bytearray(4 * 1024 * 1024)
+    n_received = 0
+
+    while is_running.value:
+        n_received += sock.recv_into(buf)
+
+    with n_received_total.get_lock():
+        n_received_total.value += n_received
+
 
 def server(args):
-    is_running = True
-    n_received_total = 0
-    lock = threading.Lock()
-
-    def receiver(sock):
-        print(f"Connected receiver local: {sock.getsockname()} remote: {sock.getpeername()}")
-
-        buf = bytearray(4 * 1024 * 1024)
-        n_received = 0
-
-        while is_running:
-            n_received += sock.recv_into(buf)
-
-        nonlocal n_received_total
-        with lock:
-            n_received_total += n_received
-
     server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_sock.bind((socket.gethostname(), PORT))
     server_sock.listen()
@@ -47,16 +42,15 @@ def server(args):
         cmd, num_streams  = struct.unpack(">bh", control_sock.recv(3))
         assert(cmd == CMD_INIT)
 
-        is_running = True
-        n_received_total = 0
-
-        threads = []
+        procs = []
+        is_running = multiprocessing.Value("i", 1)
+        n_received_total = multiprocessing.Value("l", 0)
 
         for _ in range(num_streams):
-            data_sock, address = server_sock.accept()
-            thread = threading.Thread(target=receiver, args=(data_sock,))
-            thread.start()
-            threads.append(thread)
+            sock, address = server_sock.accept()
+            proc = multiprocessing.Process(target=receiver, args=(sock, is_running, n_received_total))
+            proc.start()
+            procs.append(proc)
 
         control_sock.send(struct.pack(">b", CMD_START))
 
@@ -64,77 +58,74 @@ def server(args):
         print("Test finished, shutting down receivers")
 
         assert(cmd == CMD_END)
-        is_running = False
+        is_running.value = 0
 
-        for thread in threads:
-            thread.join()
+        for proc in procs:
+            proc.join()
 
-        print(f"Total bytes received: {n_received_total / 1000 / 1000 / 1000:.3f} GB")
+        print(f"Total bytes received: {n_received_total.value / 1000 / 1000 / 1000:.3f} GB")
+
+
+def sender(sock, is_running, n_sent_total):
+    n_sent = 0
+
+    tmp = tempfile.TemporaryFile()
+    tmp.truncate(10 * 1024 * 1024)
+
+    while not is_running.value:
+        time.sleep(0.01)
+
+    while is_running.value:
+        tmp.seek(0)
+        sock.sendfile(tmp)
+        n_sent += tmp.tell()
+
+    tmp.close()
+
+    with n_sent_total.get_lock():
+        n_sent_total.value += n_sent
 
 
 def client(args):
-    is_running = False
-    n_sent_total = 0
-    lock = threading.Lock()
-
-    def sender(sock):
-        n_sent = 0
-
-        tmp = tempfile.TemporaryFile()
-        tmp.truncate(10 * 1024 * 1024)
-
-        while not is_running:
-            time.sleep(0.01)
-
-        while is_running:
-            tmp.seek(0)
-            sock.sendfile(tmp)
-            n_sent += tmp.tell()
-
-        tmp.close()
-
-        nonlocal n_sent_total
-        with lock:
-            n_sent_total += n_sent
-
     control_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     control_sock.connect((args.client, PORT))
     control_sock.sendall(struct.pack(">bh", CMD_INIT, args.parallel))
 
-    threads = []
+    procs = []
+    is_running = multiprocessing.Value("i", 1)
+    n_sent_total = multiprocessing.Value("l", 0)
 
     for _ in range(args.parallel):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((args.client, PORT))
         print(f"Connected sender local: {sock.getsockname()} remote: {sock.getpeername()}")
 
-        thread = threading.Thread(target=sender, args=(sock,))
-        thread.start()
-        threads.append(thread)
+        proc = multiprocessing.Process(target=sender, args=(sock, is_running, n_sent_total))
+        proc.start()
+        procs.append(proc)
 
     cmd, = struct.unpack(">b", control_sock.recv(1))
     assert(cmd == CMD_START)
 
     print("All streams established, starting measurement")
 
-    is_running = True
     start_time = time.monotonic()
 
     while time.monotonic() < start_time + DURATION:
         print("Running...")
         time.sleep(1)
 
-    is_running = False
+    is_running.value = 0
     end_time = time.monotonic()
 
-    for thread in threads:
-        thread.join()
+    for proc in procs:
+        proc.join()
 
     control_sock.sendall(struct.pack(">b", CMD_END))
 
-    print(f"Total bytes sent: {n_sent_total / 1000 / 1000 / 1000:.3f} GB")
+    print(f"Total bytes sent: {n_sent_total.value / 1000 / 1000 / 1000:.3f} GB")
 
-    bw = n_sent_total / (end_time - start_time)
+    bw = n_sent_total.value / (end_time - start_time)
     print(f"Effective throughput: {bw / 1000 / 1000 / 1000 * 8:.3f} Gbps")
 
 
